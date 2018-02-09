e72f289a85460b75625c2cabaa962a9a83809138
Matthew Ishige <matthewlujp@gmail.com>
2018-02-09 10:58:56 +0900

    Tweak config in example.py

--- scripts/example.py
+++ scripts/example.py
@@ -9,12 +9,16 @@ if __name__ == '__main__':
         "sp_natural_length": 0.7,
         "sp_k": 80.0,
         "dp_c": 10.0,
-        "horizon_ts_k": 10.,
+        "horizon_ts_k": 0.,
         "vertical_ts_k": 80.,
-        "realtime_tunable_ts_rom": np.pi / 6.,
-        "static_friction_coeff": 0.7,
+        "realtime_tunable_ts_rom": np.pi * 1. / 3.,
+        "static_friction_coeff": 1.0,
         "dynamic_friction_coeff": 0.1,
-        "viscosity_friction_coeff": 5.0,
+        "viscosity_friction_coeff": 10.0,
+        "tip_sub_static_friction_coeff": 0.1,
+        "tip_sub_dynamic_friction_coeff": 0.01,
+        "tip_sub_viscosity_friction_coeff": 1.0,
+        "friction_switch_tan": np.tan(np.pi * 1. / 3.),
     }
     c = caterpillar.Caterpillar(5, (1,2,3), caterpillar_params)
 
